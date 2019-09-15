7d66bc8633ff4da97e1336947433ecd39c497d7b
Tango Gatdet Tew <tangogatdet76@gmail.com>
2019-09-14 19:48:38 -0500

    fix: fModifying code to be more cleaner

--- script.js
+++ script.js
@@ -1,45 +1,41 @@
-var col1 = document.getElementById("col1");
-var col2 = document.getElementById("col2");
-var h3 = document.querySelector("h3");
-var body = document.getElementById('body');
+//codding everything using ES6
+const col1 = document.getElementById("col1");
+const col2 = document.getElementById("col2");
+const h3 = document.querySelector("h3");
+const body = document.getElementById('body');
 
 //load the background as the input value
-body.style.background = "linear-gradient(to right, " +
-                                          col1.value +
-                                          "," +
-                                          col2.value +
-                                          ")";
+body.style.background = `linear-gradient(to right,${col1.value},
+                                          ${col2.value})`;
 //writting the value's of the input
-h3.textContent = body.style.background +";";
-var bt = document.createElement("button")
-bt.append(document.createTextNode("generate Random"));
-body.appendChild(bt)
+h3.textContent = `${body.style.background};`;
+const bt = document.createElement("button")
+bt.append(document.createTextNode("Random Background"));
+bt.classList.add("button1");
+body.appendChild(bt);
 
-
-function gen_random(){
-  var m = Math.floor(Math.random() * 256);
-  return m;
+const gen_random = () => {
+  return Math.floor(Math.random() * 256);
 }
 
-function randomNum(){
-  var gen_number = "linear-gradient(to right, rgb("
-                                    + gen_random() + ","
-                                    + gen_random() +","
-                                    + gen_random() + "), rgb("
-                                    + gen_random() + ","
-                                    + gen_random() + ","
-                                    + gen_random() + "))";
-  body.style.background = gen_number;
-  h3.textContent = gen_number +";";
+const randomNum = () => {
+  let gen_number = `linear-gradient(to right, rgb(${gen_random()},
+                                                  ${gen_random()},
+                                                  ${gen_random()}),
+                                                rgb(${gen_random()},
+                                                    ${gen_random()},
+                                                    ${gen_random()})
+                                      )`;
+   body.style.background = gen_number;
+   h3.textContent = `${gen_number};`
 }
 
-function setBack(){
-  color1 = col1.value;
-  body.style.background = "linear-gradient(to right," + color1
-  +"," + col2.value + ")";
-  h3.textContent = body.style.background + ";"
-}
+const setBack = () => {
+  body.style.background = `linear-gradient(to right, ${col1.value},
+                          ${col2.value})`;
 
+  h3.textContent = `${body.style.background};`;
+}
 
 bt.addEventListener("click", randomNum);
 col1.addEventListener("input", setBack);
